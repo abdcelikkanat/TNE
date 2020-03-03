f8fcf98902086b8a6675137ff957bcab53aadcf5
Abdulkadir Çelikkanat <abdcelikkanat@gmail.com>
2020-03-03 14:15:54 +0100

    files have been deleted

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from . import ext
-from . import tne
-from . import utils
-from . import settings
--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import dirname
-
-BASE_FOLDER = dirname(os.path.abspath(__file__))
-EXT_LIBRARY_FOLDER = "ext"
-GIBBSLDA_PATH = os.path.join(BASE_FOLDER, EXT_LIBRARY_FOLDER, "gibbslda", "lda")
-BIGCLAM_PATH = os.path.join(BASE_FOLDER, EXT_LIBRARY_FOLDER, "agm-package", "bigclam", "bigclam")
-DATASET_FOLDER = os.path.join(BASE_FOLDER, "datasets")
-OUTPUT_FOLDER = os.path.join(BASE_FOLDER, "outputs")
